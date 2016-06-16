d93884dc164b7c929b78163c9b70aa4500ea8a08
Annemarie Kohler <annie@designmynight.com>
2016-06-16 13:23:25 +0100

    calculates tax and adds it to a the total price

--- src/Receipt.php
+++ src/Receipt.php
@@ -8,13 +8,27 @@ class Receipt
     $this->order = $order;
     $this->menu = $this->convertPriceList($menu);
     $this->prices = $this->menu[0]["prices"][0];
+    $this->tax = 8.64;
   }
 
-  public function calculatePrice()
+  public function calculateNetPrice()
   {
     return array_reduce($this->order, function($carry, $item){
       return $carry + $this->prices[$item];
     });
+  }
+
+  public function calculateTotalPrice()
+  {
+    $netPrice = $this->calculateNetPrice();
+    $tax = $this->calculateTax($netPrice);
+    return $netPrice + $tax;
+  }
+
+  private function calculateTax($netPrice)
+  {
+    $tax = $this->tax / 100 * $netPrice;
+    return round($tax, 2);
   }
 
   private function convertPriceList($data)
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -8,6 +8,7 @@ class ReceiptTest extends TestCase
 {
   public $list = [["prices" => [["Cafe Latte" => 4.75, "Choc Mudcake" => 6.40]]]
   ];
+  public $receipt;
 
   public function setUp()
   {
@@ -18,8 +19,12 @@ class ReceiptTest extends TestCase
 
   public function testCalculatePrice()
   {
-    $this->assertSame(11.15, $this->receipt->calculatePrice());
+    $this->assertSame(11.15, $this->receipt->calculateNetPrice());
   }
 
-
+  public function testCalculateTotalPrice()
+  {
+    $totalExpected = round(12.11, 2);
+    $this->assertSame($totalExpected, $this->receipt->calculateTotalPrice());
+  }
 }
